72c80d8520dd4ce7b57db16986a32cf3ce1ff49b
mr10us <minenkoduck@gmail.com>
2024-04-17 20:30:36 +0300

    ..

--- web_client/src/pages/StartupMenu.jsx
+++ web_client/src/pages/StartupMenu.jsx
@@ -3,21 +3,29 @@ import { routes } from "@consts";
 
 export const StartupMenu = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-8">Добро пожаловать в фитнес-приложение!</h1>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <button className="btn-grid" onClick={() => console.log('Выбрана тренировка')}>
-          Выбрать тренировку
-        </button>
-        <button className="btn-grid" onClick={() => console.log('Калькулятор калорий')}>
-          Калькулятор калорий
-        </button>
-        <button className="btn-grid" onClick={() => console.log('Настройки')}>
-          Настройки
-        </button>
-        <button className="btn-grid" onClick={() => console.log('Выход')}>
-          Выйти
-        </button>
+    <div className="min-h-screen bg-gradient-to-b from-orange-500 to-orange-700 flex items-center justify-center">
+      <div className="bg-white p-8 md:p-12 rounded-lg shadow-lg max-w-lg">
+        <div className="text-center">
+          <img
+            src="/public/logo-bird.png"
+            alt="Logo bird"
+            className="h-40 mx-auto mb-4"
+          />
+          <h1 className="text-3xl font-bold text-gray-800 mb-2">
+            Вітаємо у Грифф
+          </h1>
+          <p className="text-gray-600 mb-6">
+            Я Ваш віртуальний тренер і готовий вести Вас до нових перемог
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <Link
+            className="bg-orange-500 text-white text-center px-6 py-3 rounded-md hover:bg-orange-600 focus:outline-none"
+            to={routes.HOME}
+          >
+            Почати тренування
+          </Link>
+        </div>
       </div>
     </div>
   );
